95874b1fd6ba086d193fb0a0813fee7efa1f7fdb
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-02-10 17:52:46 -0700

    added modal and tweaked CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-// super awesome javascript stuff here
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+var submit = document.getElementsByClassName("submit")[0];
+
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+
+submit.onclick = function() {
+let favFood = document.getElementById("fav-food").favFood;
+localStorage.setItem("textareaValue", favFood);
+
+}
