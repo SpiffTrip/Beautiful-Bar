3e14640006dcbb9f840329bc85dcae1ae0ce6c78
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-02-13 18:43:21 -0700

    added random food generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,46 @@ let favFood = document.getElementById("fav-food").favFood;
 localStorage.setItem("textareaValue", favFood);
 
 }
+
+submit.onclick = function () {
+    let favFood = document.getElementById("fav-food").favFood;
+    localStorage.setItem("textareaValue", favFood);
+  
+  }
+  const get_meal_btn = document.getElementById("get_meal");
+  const meal_container = document.getElementById("meal");
+  
+  /**Fetch the Api when cliking the button. */
+  get_meal_btn.addEventListener("click", () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then(res => res.json())
+      .then(res => {
+        createMeal(res.meals[0]);
+      })
+      .catch(e => console.error(e));
+  });
+  
+  //Create createMeal
+  const createMeal = meal => {
+  
+  
+    const newInnerHtml = `
+      <div class="row">
+        <div class="columns">
+          <img src="${meal.strMealThumb}" alt="MealImage">
+          
+          
+          
+          <ul>
+            
+          </ul>
+        </div>
+        <div class="columns seven">
+          <h4>${meal.strMeal}</h4>
+          
+        </div>
+      </div>
+      
+    `;
+    meal_container.innerHTML = newInnerHtml;
+  };
